f8928445d940c057e85cc273ffda8db238820024
Jennifer Yien <jenniferllwu@gmail.com>
2015-09-19 00:33:26 -0400

    styling

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
-  window.alert("Hi!");
+
+});
+
+$(window).resize(function(){
+   var width = $(window).width();
+   if(width >= 700 && width <= 900){
+       $('#media').removeClass('three_fourth').addClass('full_page');
+   }
 })
+.resize();
