8527776cb98da84d12b26dc9498f733c827d266a
Jennifer Yien <jenniferllwu@gmail.com>
2015-09-20 15:17:46 -0400

    code edit

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,30 +1,17 @@
 $(document).ready(function(){
-      // $('svg')
-      //   .mouseenter(function(){
-      //   $('#leftHorn').attr('class',"rotate");
-      //   })
-      //   .mouseleave(function(){
-      //   $('#leftHorn').removeClass("rotate");
-      //   $('#leftHorn').attr('class',"rerotate");
-      //   console.log('rmove')
-      //   });
-          function Horn(){
-            $('#leftHorn').attr('class',"rotate");
-            $('#leftHorn').removeClass("rerotate");
-            $('#rightHorn').removeClass("opprotate");
-            $('#rightHorn').attr('class',"rerotate");
-          }
-          function HornAgain(){
-            $('#leftHorn').removeClass("rotate");
-            $('#leftHorn').attr('class',"rerotate");
-            $('#rightHorn').removeClass("rerotate");
-            $('#rightHorn').attr('class',"opprotate");
-          }
-
+      // makes the horm wiggle
+      function Horn(){
+        $('#leftHorn').attr('class',"rotate");
+        $('#leftHorn').removeClass("rerotate");
+        $('#rightHorn').removeClass("opprotate");
+        $('#rightHorn').attr('class',"rerotate");
+      };
+      function HornAgain(){
+        $('#leftHorn').removeClass("rotate");
+        $('#leftHorn').attr('class',"rerotate");
+        $('#rightHorn').removeClass("rerotate");
+        $('#rightHorn').attr('class',"opprotate");
+      };
       setInterval(Horn, 1000);
       setInterval(HornAgain, 1600);
-      // setInterval(leftHorn, 2000);
-
-
-
 });
